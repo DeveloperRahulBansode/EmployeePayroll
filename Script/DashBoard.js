5b29b17f8e662464e62c3b83e858b01e7a411af6
DeveloperRahulBansode <rahulprofessionalid@gmail.com>
2025-04-08 17:11:52 +0530

    [Rahul] Update:Update Code and Add json server and ajex Post Data in Json File from Register Form

--- Script/DashBoard.js
+++ Script/DashBoard.js
@@ -2,6 +2,97 @@
 //     window.location.href = 'Registration.html';
 // });
 
+$(document).ready(function () {
+    $('.add-user-button').click(function () {
+        window.location.href = 'Registration.html';
+    });
+});
+
+
+$(document).ready(function () {
+
+    const employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
+    const tableBody = $("#tableBody");
+  
+    employeeList.forEach((emp, index) => {
+      const $row = $('<tr>');
+      const departments = Array.isArray(emp.department)? emp.department: [emp.department];
+  
+      $row.append(
+        `<td class="name-cell">
+          <img src="${emp.profile}" alt="Profile" class="table-profile-img">
+          <span>${emp.name}</span>
+        </td>
+        <td>${emp.gender}</td>
+        <td>
+        <div class="department-tags">
+          ${departments.map(
+            (dept) => `<span class="tag ${dept.toLowerCase()}-tag">${dept}</span>`
+          ).join("")}
+        </div>
+        </td>
+        <td>${emp.salary}</td>
+        <td>${emp.startDate.day} ${emp.startDate.month} ${emp.startDate.year}</td>
+        <td class="actions-cell">
+          <span class="material-icons action-icon delete-icon" onclick="deleteEmployee(${index})">delete</span>
+          <span class="material-icons action-icon edit-icon" onclick="editEmployee(${index})">edit</span>
+        </td>`
+      );
+  
+      tableBody.append($row);
+    });
+  
+  });
+
+  function deleteEmployee(index) {
+    const employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
+  
+    if (index >= 0 && index < employeeList.length) {
+      employeeList.splice(index, 1); 
+      localStorage.setItem("employeeList", JSON.stringify(employeeList)); 
+      location.reload(); 
+    }
+  }
+
+
+// Future User
+
+// $(document).ready(function () {
+
+//     const employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
+//     const tableBody = $("#tableBody");
+
+//     employeeList.fetch((emp, index) => {
+
+//         const $row = $('<tr>');
+
+//         $row.append(
+//             `<td class="name-cell">
+//              <img src="${emp.profile}" alt="Profile" class="table-profile-img">
+//             <span>${emp.name}</span>
+//             </td>
+//             <td>${emp.gender}</td>
+//             <td><div class="department-tags">
+//             ${emp.department.map(
+//                 (dept) =>
+//                     `<span class="tag ${dept.toLowerCase()}-tag">${dept}</span>`
+//             ).join("")}
+//             </div></td>
+//             <td>₹ ${emp.salary}</td>
+//             <td>${emp.startDate.day} ${emp.startDate.month} ${emp.startDate.year}</td>
+//             <td class="actions-cell">
+//             <span class="material-icons action-icon delete-icon" onclick="deleteEmployee(${index})">delete</span>
+//             <span class="material-icons action-icon edit-icon" onclick="editEmployee(${index})">edit</span>
+//             </td>`
+//         );
+//         tableBody.append($row);
+//     });
+
+// });
+
+
+  
+
 // window.addEventListener("DOMContentLoaded", function () {
 //     const employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
 //     const tableBody = document.getElementById("tableBody");
--- Script/Registration.js
+++ Script/Registration.js
@@ -1,26 +1,70 @@
-document.querySelector(".submit-btn").addEventListener("click", function (e) {
+// document.querySelector(".submit-btn").addEventListener("click", function (e) {
+//     e.preventDefault();
+
+
+//     const name = document.getElementById("name").value;
+
+//     const profile = document.querySelector("input[name='profile']:checked")?.nextElementSibling.className;
+
+//     const gender = document.querySelector("input[name='gender']:checked")?.nextElementSibling.innerText;
+
+//     const salary = document.getElementById("salary").value;
+
+//     const department = Array.from(document.querySelectorAll("input[name='department']:checked"))
+//       .map(chk => chk.nextElementSibling.innerText);
+
+//     const dateSelects = document.querySelectorAll(".date-select select");
+//     const startDate = {
+//       day: dateSelects[0].value,
+//       month: dateSelects[1].value,
+//       year: dateSelects[2].value,
+//     };
+
+//     const notes = document.getElementById("textarea").value;
+
+//     const employee = {
+//       name,
+//       profile,
+//       gender,
+//       salary,
+//       department,
+//       startDate,
+//       notes,
+//     };
+
+
+//     let employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
+//     employeeList.push(employee);
+
+//     localStorage.setItem("employeeList", JSON.stringify(employeeList));
+//     alert("Data saved successfully!");
+
+//     document.getElementById("form").reset();
+//   });
+
+//   document.querySelector(".reset-btn").addEventListener("click", function () {
+//     document.getElementById("form").reset();
+//   });
+
+//   document.querySelector(".cancel-btn").addEventListener("click", function () {
+//     window.location.href = "D:/Employee PayRoll/Pages/DashBoard.html"; 
+//   });
+
+$(document).ready(function () {
+  $(".submit-btn").click(function (e) {
     e.preventDefault();
 
-  
-    const name = document.getElementById("name").value;
-
-    const profile = document.querySelector("input[name='profile']:checked")?.nextElementSibling.className;
-
-    const gender = document.querySelector("input[name='gender']:checked")?.nextElementSibling.innerText;
-  
-    const salary = document.getElementById("salary").value;
-  
-    const department = Array.from(document.querySelectorAll("input[name='department']:checked"))
-      .map(chk => chk.nextElementSibling.innerText);
-  
-    const dateSelects = document.querySelectorAll(".date-select select");
+    const name = $("#name").val();
+    const profile = $("input[name='profile']:checked").next(".profile-img").css("background-image").slice(5, -2);;
+    const gender = $("input[name='gender']:checked").next().text();
+    const salary = $("#salary").val();
+    const department = $("input[name='department']:checked").map(function () {return $(this).next().text();}).get();
     const startDate = {
-      day: dateSelects[0].value,
-      month: dateSelects[1].value,
-      year: dateSelects[2].value,
+      day: $("#day").val(),
+      month: $("#month").val(),
+      year: $("#year").val(),
     };
-
-    const notes = document.getElementById("textarea").value;
+    const notes = $("#textarea").val();
 
     const employee = {
       name,
@@ -32,20 +76,65 @@ document.querySelector(".submit-btn").addEventListener("click", function (e) {
       notes,
     };
 
-  
-    let employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
-    employeeList.push(employee);
-    localStorage.setItem("employeeList", JSON.stringify(employeeList));
+
+
+      //Data Store in Local Storage
+    // let employeeList = JSON.parse(localStorage.getItem("employeeList")) || [];
+    // employeeList.push(employee);
+    // localStorage.setItem("employeeList", JSON.stringify(employeeList));
+
+
+
+    const url = "http://localhost:3000/employees";
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(employee),
+      success: (response) => {
+        console.log(`Employee Added successfully:`, response)
+        window.location.href = "dashboard.html"
+      },
+      error: (error) => {
+        console.error("Error:", error)
+        alert(`Error in Adding  employee. Please try again.`)
+      },
+    })
+
+    // $.ajax({
+    //   url: url,
+    //   type: isEdit ? "PUT" : "POST",
+    //   contentType: "application/json",
+    //   data: JSON.stringify(formData),
+    //   success: (response) => {
+    //     console.log(`Employee ${isEdit ? 'updated' : 'added'} successfully:`, response)
+    //     window.location.href = "dashboard.html"
+    //   },
+    //   error: (error) => {
+    //     console.error("Error:", error)
+    //     alert(`Error ${isEdit ? 'updating' : 'adding'} employee. Please try again.`)
+    //   },
+    // })
+
     alert("Data saved successfully!");
-    document.getElementById("form").reset();
+    $("#form")[0].reset();
   });
 
-  document.querySelector(".reset-btn").addEventListener("click", function () {
-    document.getElementById("form").reset();
-  });
-  
-  document.querySelector(".cancel-btn").addEventListener("click", function () {
-    window.location.href = "D:/Employee PayRoll/Pages/DashBoard.html"; 
+  $(document).ready(function (e) {
+    $(".reset-btn").click(function () {
+      $("#form")[0].reset();
+    });
   });
 
-  
+  $(document).ready(function (e) {
+    $(".cancel-btn").click(function () {
+      window.location.href = "D:/Employee PayRoll/Pages/DashBoard.html";
+    });
+  });
+
+});
+
+
+
+
